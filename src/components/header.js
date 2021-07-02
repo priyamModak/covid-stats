8073c58a78cd9f61111e62148446d352b28588e3
Priyam Modak <priyam.inferno@gmail.com>
2021-07-02 22:49:40 +0530

    Dropdown structure fixed

--- src/components/header.js
+++ src/components/header.js
@@ -10,11 +10,10 @@ const Header = () => {
 
         const countryApi = await Axios.get("https://restcountries.eu/rest/v2/all");
         let list = countryApi.data;
-        console.log(list);
-        for (const eachItem of list) {
-            setAllCountries([...allCountries, eachItem]);
-        }
-        console.log(allCountries);
+        list.forEach(element => {
+            setAllCountries([...allCountries, element])
+        });
+        // console.log(list);
     }
 
     useEffect(() => {
@@ -24,21 +23,21 @@ const Header = () => {
     return (
         <div className="theme-bg">
             <Navbar expand="md">
-                <NavbarBrand href="/">
+                <NavbarBrand href="/" style={{marginLeft: "1vw"}}>
                     <h3 className="text-muted">Covid Statistics</h3>
                 </NavbarBrand>
+                {
+                    allCountries.map(item => [
                 <UncontrolledDropdown style={{right: "2vw", position: "absolute"}}>
                     <DropdownToggle caret>
                         Country
                     </DropdownToggle>
                     <DropdownMenu>
-                        {
-                            allCountries.map(item => {
-                                <DropdownItem key={item.area}>{item.name}</DropdownItem>
-                            })
-                        }
+                        <DropdownItem>{item.name}</DropdownItem>
                     </DropdownMenu>
                 </UncontrolledDropdown>
+                ])
+                }
             </Navbar>
         </div>
     )
