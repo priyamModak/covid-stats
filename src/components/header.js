1ab89731759e2be9f3fb091ff32d5813155dbab3
Priyam Modak <priyam.inferno@gmail.com>
2021-07-14 22:40:33 +0530

    Issue resolved

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,12 @@ import Axios from "axios";
 const Header = () => {
 
     const [allCountries, setAllCountries] = useState([]);
-    
+
     var getAllCountries = async () => {
 
         const countryApi = await Axios.get("https://restcountries.eu/rest/v2/all");
         let list = countryApi.data;
-        list.forEach(element => {
-            setAllCountries([...allCountries, element])
-        });
-        // console.log(list);
+        setAllCountries([...list]);
     }
 
     useEffect(() => {
@@ -23,21 +20,21 @@ const Header = () => {
     return (
         <div className="theme-bg">
             <Navbar expand="md">
-                <NavbarBrand href="/" style={{marginLeft: "1vw"}}>
+                <NavbarBrand href="/" style={{ marginLeft: "1vw" }}>
                     <h3 className="text-muted">Covid Statistics</h3>
                 </NavbarBrand>
-                {
-                    allCountries.map(item => [
-                <UncontrolledDropdown style={{right: "2vw", position: "absolute"}}>
+                <UncontrolledDropdown style={{ right: "2vw", position: "absolute" }}>
                     <DropdownToggle caret>
                         Country
                     </DropdownToggle>
                     <DropdownMenu>
-                        <DropdownItem>{item.name}</DropdownItem>
+                        {
+                            allCountries.map(item => [
+                                <DropdownItem>{item.name}</DropdownItem>
+                            ])
+                        }
                     </DropdownMenu>
                 </UncontrolledDropdown>
-                ])
-                }
             </Navbar>
         </div>
     )
